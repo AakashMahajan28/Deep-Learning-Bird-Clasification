32c1f0ff0b0173182fc90df974e509ffafffdc50
Aakash Mahajan <aaks.11100@gmail.com>
2022-08-13 14:19:10 +0200

    Updating Files

--- app.py
+++ app.py
@@ -4,34 +4,37 @@ from keras.preprocessing import image
 import numpy as np
 
 app = Flask(__name__)
-dic = {0 : 'Bald Eagle', 1 : 'Snowy Owl'}
+
+labels_str = ['Bald Eagle', 'Snowy Owl']
 model = load_model('classification_model.h5')
 model.make_predict_function()
 
+
 def predict_label(img_path):
-    i = image.load_img(img_path, target_size=(100,100))
-    i = image.img_to_array(i)/255.0
+    i = image.load_img(img_path, target_size=(100, 100))
+    i = image.img_to_array(i) / 255.0
     i = np.expand_dims(i, axis=0)
-    p = model.predict_classes(i)
-    return dic[p[0][0]]
+    p = model.predict(i)
+    prob = int(np.round(p, 0))
+    img_class = labels_str[prob]
+    return img_class
+
 
 # routes
 @app.route("/", methods=['GET', 'POST'])
 def main():
     return render_template('index.html')
 
-@app.route("/submit", methods = ['GET', 'POST'])
+
+@app.route("/submit", methods=['GET', 'POST'])
 def get_output():
     if request.method == 'POST':
         img = request.files['my_image']
         img_path = "static/" + img.filename
         img.save(img_path)
-        p = predict_label(img_path)
-        return render_template("index.html", prediction = p, img_path = img_path)
-
-if __name__ =='__main__':
- #   app.debug = True
-    app.run(debug= False)
+        pred_label = predict_label(img_path)
+        return render_template("index.html", prediction=pred_label, img_path=img_path)
 
 
-
+if __name__ == '__main__':
+    app.run(debug=True)
